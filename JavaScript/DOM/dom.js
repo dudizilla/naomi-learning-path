6b9ae2a347b4bbdcd614edb35b4f555721e1ea5e
Naomi Mori <naomi.morisilva@gmail.com>
2026-03-16 10:16:24 -0300

    feat: EIP-37 Events and Listeners
    Signed-off-by: Naomi Mori <naomi.morisilva@gmail.com>

--- JavaScript/DOM/dom.js
+++ JavaScript/DOM/dom.js
@@ -10,3 +10,31 @@ for (let i = 0; i < allSquares.length; i++) {
   const currentElement = allSquares[i];
   currentElement.style.width = "300px";
 }
+
+const button = document.querySelector(".event-button");
+button.addEventListener(
+    "click", function () {
+        alert("Good Morning, Sunshine!");
+    }
+);
+
+const input = document.querySelector(".input-to-copy");
+const paragraph = document.querySelector(".p-to-copy-to");
+
+input.addEventListener("keyup", function () {
+  paragraph.innerText = input.value;
+});
+
+const colorBox = document.querySelector(".color-box");
+const colorInput = document.querySelector(".color-input");
+
+colorInput.addEventListener("change", function(){
+    colorBox.style.backgroundColor = colorInput.value;
+});
+
+document.querySelector(".button-container")
+  .addEventListener("click", 
+    function (event) {
+        alert(`This is ${event.target.innerText}`);
+    }
+);
