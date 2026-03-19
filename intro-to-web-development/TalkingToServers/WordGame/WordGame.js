9d0a1809c8e6e63eb14394ccadfbc2fddbe16b9b
Naomi Mori <naomi.morisilva@gmail.com>
2026-03-19 14:09:55 -0300

    feat: EIP-38 Word Game project
    add a message display component and use variables for colors
    
    Signed-off-by: Naomi Mori <naomi.morisilva@gmail.com>

--- intro-to-web-development/TalkingToServers/WordGame/WordGame.js
+++ intro-to-web-development/TalkingToServers/WordGame/WordGame.js
@@ -1,140 +1,134 @@
-//user types
-// when it reaches 5 letters and enter, validates the word
-
-// check if it's a word
-// check index by index if the letter matches
-// if it doesn't, check if there are letters that exist in the word
-
-//make array, remove item if exists in user output
-// to avois marking 2 letters that are the same even though there's only one
-
 VALIDATE_URL = "https://words.dev-apis.com/validate-word";
 WORD_URL = "https://words.dev-apis.com/word-of-the-day";
+
+const yellow = "#edca03";
+const gray = "#988d8d";
+const green = "#0b750b";
+const whiteishText = "#ebe7e7";
 
 let currentRow = 0;
 let word = "";
 let rowComplete = false;
+let gameOver = false;
 
-const letterBoxes = document.querySelectorAll('.letter-box');
+const letterBoxes = document.querySelectorAll(".letter-box");
 
-
-function isLetter(letter){
+function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
 
-function emptyWord(){
+function emptyWord() {
     word = "";
 }
 
-function removeLastLetter(){
-    word = word.substring(0,word.length-1);
+function removeLastLetter() {
+    word = word.substring(0, word.length - 1);
 }
 
-function findRowBoxes(){
+function findRowBoxes() {
     const rowStart = currentRow * 5;
     const rowBoxes = [];
 
     for (let i = rowStart; i < rowStart + 5; i++) {
-            rowBoxes.push(letterBoxes[i]);
+        rowBoxes.push(letterBoxes[i]);
     }
 
     return rowBoxes;
 }
 
-async function isWordOfTheDay(row) {
+function displayMessage(message) {
+    const infoBlock = document.querySelector(".info-block");
+    infoBlock.innerHTML = "";
+    const headingMessage = document.createElement("h2");
+    headingMessage.textContent = message;
+    infoBlock.appendChild(headingMessage);
+}
 
+async function isWordOfTheDay(row) {
     const promise = await fetch(WORD_URL);
     const response = await promise.json();
+
     const wordOfTheDay = String(response.word);
 
     const guess = word.toUpperCase();
     const answer = wordOfTheDay.toUpperCase();
 
-    console.log(`Guess is: "${guess}", Answer is: "${answer}"`); 
-    console.log(guess === answer);
-    console.log("remainingLetter before loop:", wordOfTheDay.split(""));
-
     let remainingLetter = answer.split("");
-    let boxColors = ["gray", "gray", "gray", "gray", "gray"];
+    let boxColors = [gray, gray, gray, gray, gray];
 
-    for (let i=0;i<5;i++){
-        if (guess[i] == answer[i]){
-            boxColors[i] = "green";
+    for (let i = 0; i < 5; i++) {
+        if (guess[i] == answer[i]) {
+            boxColors[i] = green;
             remainingLetter[i] = null;
         }
     }
 
-    for (let i=0;i<5;i++){
-        if (boxColors[i] === "gray"){
+    for (let i = 0; i < 5; i++) {
+        if (boxColors[i] === gray) {
             let indexAnswer = remainingLetter.indexOf(guess[i]);
-            if (indexAnswer != -1){
-                boxColors[i] = "yellow";
+            if (indexAnswer != -1) {
+                boxColors[i] = yellow;
                 remainingLetter[indexAnswer] = null;
             }
         }
     }
 
-    for (let i=0;i<5;i++){
+    for (let i = 0; i < 5; i++) {
         row[i].style.backgroundColor = boxColors[i];
-        row[i].style.color = "#ebe7e7";
+        row[i].style.color = whiteishText;
         row[i].disabled = true;
     }
 
+    const allGreen = boxColors.every((color) => color === green);
+    if (allGreen) {
+        gameOver = true;
+        displayMessage("CONGRATULATIONS, YOU WON! 🥳");
+    }
 }
 
-async function checkWord(rowBoxes){
+async function checkWord(rowBoxes) {
     console.log(`The word is ${word}`);
 
-    const promise = await fetch(VALIDATE_URL,{
+    const promise = await fetch(VALIDATE_URL, {
         method: "POST",
-        body: JSON.stringify({"word": word})
+        body: JSON.stringify({ word: word }),
     });
 
     const response = await promise.json();
 
-    if (response.validWord){
+    if (response.validWord) {
         await isWordOfTheDay(rowBoxes);
 
         currentRow++;
         emptyWord();
-        if (currentRow<6) {
+        if (currentRow < 6) {
             letterBoxes[currentRow * 5].focus();
         }
-
-    }
-
-    else{
-        console.log("Not a valid word!");
+    } else {
+        displayMessage("Not a valid word! Try again.");
         emptyWord();
 
-        for(let i=0;i<5;i++){
+        for (let i = 0; i < 5; i++) {
             rowBoxes[i].value = "";
             letterBoxes[currentRow * 5].focus();
         }
-        return; 
+        return;
     }
 }
 
-function handleLetter(event,key,index,box){
-    console.log("Matched letter!" + key);
-
+function handleLetter(event, key, index, box) {
     event.preventDefault();
 
-    if (word.length < 5){
-        word+=key;
+    if (word.length < 5) {
+        word += key;
         box.value = key;
         console.log(word);
 
-        if ((index + 1) % 5 !== 0) 
-            letterBoxes[index + 1].focus();
-    }
-    
-    else event.preventDefault();
+        if ((index + 1) % 5 !== 0) letterBoxes[index + 1].focus();
+    } else event.preventDefault();
 }
 
-function handleBackspace(index,box){
-    console.log("Backspace detected");
-
+function handleBackspace(index, box) {
     box.value = "";
 
     if (index % 5 !== 0) {
@@ -143,11 +137,9 @@ function handleBackspace(index,box){
     }
 }
 
-function handleEnter(){
-    console.log("Enter detected");
-    
+function handleEnter() {
     const rowBoxes = findRowBoxes();
-    const allFilled = rowBoxes.every(b => b.value !== '');
+    const allFilled = rowBoxes.every((b) => b.value !== "");
     console.log(allFilled);
 
     if (allFilled) {
@@ -155,33 +147,24 @@ function handleEnter(){
     }
 }
 
-letterBoxes.forEach(
-    (box, index) =>
-        {
-            box.addEventListener('keyup', function(event) {
-                const key = event.key;
+letterBoxes.forEach((box, index) => {
+    box.addEventListener("keyup", function (event) {
+        const key = event.key;
 
-                 const boxRow = Math.floor(index / 5);
-                 console.log(boxRow + " " + currentRow);
+        const boxRow = Math.floor(index / 5);
+        console.log(boxRow + " " + currentRow);
 
-                if (boxRow > currentRow || key === " "){
-                    event.preventDefault();
-                    return;
-                }
-                
-                if (isLetter(key)) {
-                    handleLetter(event,key,index,box);
-                }
-                
-                else if (key === 'Backspace') {
-                    handleBackspace(index,box);
-                }
-                
-                else if (key === 'Enter') {
-                    handleEnter();
-                }
+        if (gameOver || boxRow > currentRow || key === " ") {
+            event.preventDefault();
+            return;
+        }
 
-                else event.preventDefault();
-
+        if (isLetter(key)) {
+            handleLetter(event, key, index, box);
+        } else if (key === "Backspace") {
+            handleBackspace(index, box);
+        } else if (key === "Enter") {
+            handleEnter();
+        } else event.preventDefault();
     });
 });
