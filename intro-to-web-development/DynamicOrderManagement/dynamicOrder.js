e6ae16d58a90d7696986edb7a801227d595a5dd4
Naomi Mori <naomi.morisilva@gmail.com>
2026-03-25 15:14:08 -0300

    feat: show total values and add event listener to + and - buttons
    Signed-off-by: Naomi Mori <naomi.morisilva@gmail.com>

--- intro-to-web-development/DynamicOrderManagement/dynamicOrder.js
+++ intro-to-web-development/DynamicOrderManagement/dynamicOrder.js
@@ -20,17 +20,53 @@ const cart = [
         price: 9.9,
         quantity: 1,
     },
-    { id: 4, name: "Cookie", ingredients: [], price: 4.5, quantity: 3 },
+    //{ id: 4, name: "Cookie", ingredients: [], price: 4.5, quantity: 3 },
 ];
 
 const cartList = document.querySelector(".cart__list");
+let orderSum = 0;
+
+function showFinalValues(){
+        const subtotal = document.querySelector(".total__subtotal");
+    const subtotalValue = document.createElement("td");
+    subtotalValue.classList.add("total__price");
+    subtotalValue.innerText = orderSum.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+    subtotal.appendChild(subtotalValue);
+
+    const tax = document.querySelector(".total__tax");
+    const taxValue = document.createElement("td");
+    taxValue.classList.add("total__price");
+    taxValue.innerText = (orderSum*0.08).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+    tax.appendChild(taxValue);
+
+    const total = document.querySelector(".total__row--total");
+    const totalValue = document.createElement("td");
+    totalValue.classList.add("total__price");
+    totalValue.innerText = (orderSum*1.08).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+   total.appendChild(totalValue);
+}
 
 function renderItems(items) {
+    
     items.forEach((item) => {
         let ingredientsHTML = "";
 
         item.ingredients.forEach((ingredient) => {
             ingredientsHTML += `<p class="cart__item-spec">${ingredient}</p>`;
+        });
+
+        let price = item.price.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL",
         });
 
         const itemList = document.createElement("li");
@@ -44,12 +80,28 @@ function renderItems(items) {
             <p class="cart__item-name">${item.name}</p>
             ${ingredientsHTML}   
         </div>
-        <div class="cart__item-price">R$${item.price.toFixed(2)}</div> `;
+        <div class="cart__item-price">${price}</div> `;
 
         itemList.classList.add("cart__item");
 
         cartList.appendChild(itemList);
+        orderSum += parseInt(item.quantity) * parseFloat(item.price);
     });
 }
 
 renderItems(cart);
+showFinalValues();
+
+cartList.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        let buttonValue = event.target.innerText;
+
+        const button = event.target;
+        const parent = button.parentElement;
+        const sibling = parent.querySelector("span");
+        console.log(sibling, sibling.innerText)
+        if (buttonValue === "+") {
+            console.log("aqui aumenta");
+        } else console.log("aqui abaixa");
+    }
+});
