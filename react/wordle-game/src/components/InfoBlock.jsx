3103f1e2bdb8573e36614598ef2756616daf93da
Naomi Mori <78877836+naomi-morii@users.noreply.github.com>
2026-04-10 11:58:17 -0300

    Apply suggestion from @dudizilla to use correct syntax
    Co-authored-by: Maria Eduarda Cunha <42151808+dudizilla@users.noreply.github.com>

--- react/wordle-game/src/components/InfoBlock.jsx
+++ react/wordle-game/src/components/InfoBlock.jsx
@@ -3,7 +3,7 @@ import "@/styles/InfoBlock.css"
 export default function InfoBlock({loading, displayMessage}) {
     return (
         <div className="info-block">
-            {loading && <span className="loader">  </span>}
+            {loading && <span className="loader" />}
             {!loading && displayMessage ? <h2 className="info-block__title"> {displayMessage} </h2> : null}
         </div>
     );
