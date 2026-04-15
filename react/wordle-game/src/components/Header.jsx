094192b3d4e2437dca9cb47a04a5edc95d36654e
Naomi Mori <naomi.morisilva@gmail.com>
2026-04-15 16:59:59 -0300

    feat: add settings icon
    Signed-off-by: Naomi Mori <naomi.morisilva@gmail.com>

--- react/wordle-game/src/components/Header.jsx
+++ react/wordle-game/src/components/Header.jsx
@@ -3,7 +3,9 @@ import "@/styles/Header.css";
 export default function Header() {
     return (
         <header className="page-header">
+             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,400,1,0&icon_names=settings" />
             <h1 className="page-header__title"> Wordle </h1>
+            <span class="material-symbols-outlined">settings</span>
         </header>
     );
 }
