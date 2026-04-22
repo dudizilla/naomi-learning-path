d17942843b18752f4e8da03c39dadf14a1e59811
Naomi Mori <naomi.morisilva@gmail.com>
2026-04-22 14:52:13 -0300

    feat: jsx in react section
    Signed-off-by: Naomi Mori <naomi.morisilva@gmail.com>

--- react/complete-react-frontendmasters/padre-ginos/eslint.config.mjs
+++ react/complete-react-frontendmasters/padre-ginos/eslint.config.mjs
@@ -1,12 +1,20 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 /** @type (import('eslint').Linter.Config) */
 export default [
   js.configs.recommended,
   {
-    files: ["**/*.js"],
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -14,6 +22,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
