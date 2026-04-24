9b0e94414441e8bd836a3f7a4a0845dc12194b18
Naomi Mori <naomi.morisilva@gmail.com>
2026-04-24 14:36:17 -0300

    feat: add CartContext with useContext
    Signed-off-by: Naomi Mori <naomi.morisilva@gmail.com>

--- react/complete-react-frontendmasters/padre-ginos/src/Cart.jsx
+++ react/complete-react-frontendmasters/padre-ginos/src/Cart.jsx
@@ -5,7 +5,6 @@ const intl = new Intl.NumberFormat("en-US", {
 
 export default function Cart({ cart, checkout }) {
   let total = 0;
-
   for (let i = 0; i < cart.length; i++) {
     const current = cart[i];
     total += current.pizza.sizes[current.size];
